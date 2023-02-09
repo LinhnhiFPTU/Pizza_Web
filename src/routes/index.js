dfad8e24e82f10c13e3e295a927a46415ae1db5b
minhhuanne <huandmse171114@fpt.edu.vn>
2023-02-09 15:19:45 +0700

    cart page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Custom from '../pages/Custom';
 import About from '../pages/About';
+import Cart from '../pages/Cart/Cart.js';
 import ContentOnlyLayout from '../components/Layout/ContentOnlyLayout';
 
 const publicRoutes = [
@@ -11,6 +12,7 @@ const publicRoutes = [
     { path: '/register', component: Register, layout: ContentOnlyLayout },
     { path: '/customize', component: Custom },
     { path: '/about-us', component: About },
+    { path: '/cart', component: Cart },
 ];
 
 const privateRoutes = [];
