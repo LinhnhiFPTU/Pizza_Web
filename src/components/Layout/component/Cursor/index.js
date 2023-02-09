2e884aa63bf733dc68706a4d42486c201fc8a188
minhhuanne <huandmse171114@fpt.edu.vn>
2023-02-09 20:18:34 +0700

    mouse click animation

--- src/components/Layout/component/Cursor/index.js
+++ src/components/Layout/component/Cursor/index.js
@@ -20,9 +20,41 @@ function Cursor() {
         document.addEventListener('mouseout', () => {
             cursor.current.style.display = 'none';
         });
+
+        document.addEventListener('click', (e) => {
+            let cursorClone = cursor.current.cloneNode(true);
+            cursorClone.classList.add(cx('active'));
+            console.log(cursorClone);
+            document.querySelector('body').appendChild(cursorClone);
+            setTimeout(() => {
+                cursorClone.remove();
+            }, 1000);
+        });
     }, []);
 
-    return <div className={cx('cursor')} ref={cursor}></div>;
+    return (
+        <div className={cx('cursor')} ref={cursor}>
+            <div className={cx('click-animation')}>
+                <div
+                    className={`${cx('shape')} ${cx('circle')} ${cx('big')}`}
+                ></div>
+                <div
+                    className={`${cx('shape')} ${cx('circle')} ${cx('small')}`}
+                ></div>
+                <div
+                    className={`${cx('shape')} ${cx('triangle')} ${cx(
+                        'yellow',
+                    )}`}
+                ></div>
+                <div
+                    className={`${cx('shape')} ${cx('triangle')} ${cx(
+                        'green',
+                    )}`}
+                ></div>
+                <div className={`${cx('shape')} ${cx('disc')} `}></div>
+            </div>
+        </div>
+    );
 }
 
 export default Cursor;
--- src/context/Context.js
+++ src/context/Context.js
@@ -1,32 +1,30 @@
-import { createContext, useContext, useReducer } from "react";
-import faker from "faker";
-import { cartReducer } from "./Reducers";
+import { createContext, useContext, useReducer } from 'react';
+// import faker from 'faker';
+import { cartReducer } from './Reducers';
 
 const Cart = createContext();
-faker.seed(99);
+// faker.seed(99);
 
 const Context = ({ children }) => {
-  const products = [...Array(20)].map(() => ({
-    id: faker.datatype.uuid(),
-    name: faker.commerce.productName(),
-    price: faker.commerce.price(),
-    image: faker.random.image(),
-  }));
+    const products = [...Array(20)].map(() => ({
+        // id: faker.datatype.uuid(),
+        // name: faker.commerce.productName(),
+        // price: faker.commerce.price(),
+        // image: faker.random.image(),
+    }));
 
-  const [state, dispatch] = useReducer(cartReducer, {
-    products: products,
-    cart: [],
-  });
+    const [state, dispatch] = useReducer(cartReducer, {
+        products: products,
+        cart: [],
+    });
 
-  return (
-    <Cart.Provider value={{ state, dispatch }}>
-      {children}
-    </Cart.Provider>
-  );
+    return (
+        <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>
+    );
 };
 
 export const CartState = () => {
-  return useContext(Cart);
+    return useContext(Cart);
 };
 
 export default Context;
